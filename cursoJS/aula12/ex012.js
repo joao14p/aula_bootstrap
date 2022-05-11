08342bd8aacc6896941704a33516298d52b4e8c8
Joao Paulo <joaopaulodantasbernardo@gmail.com>
2022-05-11 10:43:31 -0300

    Aula 12 Java Script

--- cursoJS/aula12/ex012.js
+++ cursoJS/aula12/ex012.js
@@ -1,4 +1,5 @@
-var hora = 03
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas.`)
 if (hora >= 00 && hora < 5) {
     console.log('Boa Madrugada!')
