b092efc9afaee2f4f9283601853165aa15a3f128
Joao Paulo <joaopaulodantasbernardo@gmail.com>
2022-05-11 10:25:47 -0300

    update

---
+++ cursoJS/aula12/ex012.js
@@ -0,0 +1,11 @@
+var hora = 03
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora >= 00 && hora < 5) {
+    console.log('Boa Madrugada!')
+} else if (hora >= 5 && hora < 13) {
+    console.log('Bom Dia!')
+} else if (hora >= 13 && hora < 19) {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
